424d015d9c6ab39c5273d32b61e0ef273117613f
ATHEXgod <moditrathor@gmail.com>
2024-07-24 18:43:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://moditrathor:<password>@cluster0.sdkc4bt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://moditrathor:@cluster0.sdkc4bt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "ultroidxTeam")
 
 #Shortner (token system) 
